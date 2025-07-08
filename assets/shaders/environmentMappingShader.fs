c759856c98e609e0d503231908f254bb4e34056b
uganh16 <uganh16@gmail.com>
2025-07-09 01:20:42 +0800

    Implement reflection via environment mapping

---
+++ assets/shaders/environmentMappingShader.fs
@@ -0,0 +1,17 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 fragPos;
+in vec3 fragNormal;
+
+uniform vec3 cameraPos;
+
+uniform samplerCube skybox;
+
+void main() {
+  vec3 V = normalize(fragPos - cameraPos);
+  vec3 N = normalize(fragNormal);
+  vec3 R = reflect(V, N);
+  FragColor = vec4(texture(skybox, R).rgb, 1.0);
+}
